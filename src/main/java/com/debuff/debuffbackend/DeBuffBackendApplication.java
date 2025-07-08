842d7402d694e5f1cfdb15f67f22b7de47851cf3
qfl12 <m18228724230@163.com>
2025-07-08 21:46:57 +0800

    创建项目

---
+++ src/main/java/com/debuff/debuffbackend/DeBuffBackendApplication.java
@@ -0,0 +1,13 @@
+package com.debuff.debuffbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeBuffBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeBuffBackendApplication.class, args);
+    }
+
+}
